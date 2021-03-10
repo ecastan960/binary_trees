12acaa7e890608fea90b2f4b07316de7916aafcb
ecastan960 <ecastan1@eafit.edu.co>
2021-03-10 17:55:56 -0500

    11|First test

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - function that counts the leaves in a binary tree
+ *
+ * @tree: Pointer to the root node of the tree to traverse
+ * Return: Number of leaves in a tree
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	else
+		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+}
